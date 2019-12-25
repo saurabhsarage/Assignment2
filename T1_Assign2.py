77ca5e307ede84f761684b9f1a9ec27e88f0bb99
Saurabh Sarage <57210145+saurabhsarage@users.noreply.github.com>
2019-12-25 15:15:20 +0530

    Create T1_Assign2.py

---
+++ T1_Assign2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 14:36:10 2019
+
+@author: User
+
+Implement List comprehensions to produce the following lists.
+Write List comprehensions to produce the following Lists
+['A', 'C', 'A', 'D', 'G', 'I', ’L’, ‘ D’]
+['x', 'xx', 'xxx', 'xxxx', 'y', 'yy', 'yyy', 'yyyy', 'z', 'zz', 'zzz', 'zzzz']
+['x', 'y', 'z', 'xx', 'yy', 'zz', 'xx', 'yy', 'zz', 'xxxx', 'yyyy', 'zzzz']
+[[2], [3], [4], [3], [4], [5], [4], [5], [6]]
+[[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
+[(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]
+
+"""
+
+l1 = [l for l in 'ACADGILD']
+ll = ['x','y','z']
+l2 = [l* i for i in range(1,5) for l in ll ]
+l3 = [l for l in ll ]
+print(l1)
+print(l2)
+print(l3)
